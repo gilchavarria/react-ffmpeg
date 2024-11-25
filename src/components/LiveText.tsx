fe1b1f9eed9f8aba0e66bd274e94e803dcad272d
gilchavarria <gilbert@fuzzyredpanda.com>
2024-11-25 09:35:07 -0700

    Fixed declaration error

---
+++ src/components/LiveText.tsx
@@ -0,0 +1,12 @@
+
+type LiveTextProps = {
+    name: string
+}
+
+export const LiveText = (props: LiveTextProps) => {
+    return (
+        <div>
+            <h2>Welcome {props.name}! You have 10 unread messages</h2>
+        </div>
+    )
+}
